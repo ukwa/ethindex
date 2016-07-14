b87787ea6f885a6499811d633b5962d8bfb5e02f
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-07-14 18:41:45 +0100

    A Solr instance.

--- create_solr_core.sh
+++ create_solr_core.sh
@@ -1,1 +1,1 @@
-docker exec -it --user=solr composepulsecrawler_crawl_state_solr_1 bin/solr create_core -c crawl_state
+docker exec -it --user=solr ethindex_ethos_solr_1 bin/solr create_core -c ethindex
